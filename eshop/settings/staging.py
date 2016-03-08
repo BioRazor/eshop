f042ae949a498bdfcb0eb81d2627b4f040f04185
D3APC <BioRazor.a@gmail.com>
2016-03-07 19:57:46 -0430

    Archivos estaticos

--- eshop/settings/staging.py
+++ eshop/settings/staging.py
@@ -18,10 +18,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR.child('static'),)
+"""
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
